586c9be99f2430ef075982fb45d5893f93d178c2
anhha77 <kentucky772001@gmail.com>
2024-10-16 23:24:02 +0700

    update

--- routes/admin/category.api.js
+++ routes/admin/category.api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const categoryController = require("../../controllers/admin/category.controller");
 const authentication = require("../../middlewares/authentication");
 const validators = require("../../middlewares/validators");
-const { body } = require("express-validator");
+const { body, param } = require("express-validator");
 const { CategoryType } = require("../../helpers/constant");
 
 /**
@@ -31,6 +31,24 @@ router.post(
       ),
   ]),
   categoryController.createCategory
+);
+
+/**
+ * @route PUT admin/category/:categoryId
+ * @description Update a category
+ * @body {parentCategoryId, categoryName}
+ * @access Login required (Admin)
+ */
+
+router.put(
+  "/:categoryId",
+  authentication.loginRequired,
+  (req, res, next) => authentication.validateRole(req, res, next, ["admin"]),
+  validators.validate([
+    param("categoryId").exists().isString().custom(validators.checkObjectId),
+    body("parentCategoryId").optional().custom(validators.checkObjectId),
+    body("categoryName").optional().notEmpty().isString(),
+  ])
 );
 
 module.exports = router;
